a1aabd4b12038cda7fcc24d8c7023e85523a35ba
ritiger <jinzhexing26@gmail.com>
2021-10-16 02:17:57 +0800

    first commit;

---
+++ application/controllers/Auth.php
@@ -0,0 +1,43 @@
+<?php
+
+class Auth extends CI_Controller{
+
+    public function register(){
+
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_message('is_unique', 'Email address already exist, please try another.');
+        $this->form_validation->set_message('is_unique', 'Phone number already exist, please try another.');
+
+        $this->form_validation->set_rules('first_name', 'First name', 'required');
+        $this->form_validation->set_rules('last_name', 'Last name', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('phone', 'Phone', 'required|is_unique[users.phone]');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+
+
+        if($this->form_validation->run() == false) {
+            // Here we will load our view
+            $this->load->view('register');
+        } else {
+
+            // Here we will save user record in db
+
+            $this->load->model('Auth_model');
+            $formArray = array();
+            $formArray['first_name'] = $this->input->post('first_name');
+            $formArray['last_name'] = $this->input->post('last_name');
+            $formArray['email'] = $this->input->post('email');
+            $formArray['phone'] = $this->input->post('phone');
+            $formArray['password'] = md5($this->input->post('password'));
+            $formArray['created_at'] = date('Y-m-d H:i:s');
+            // $formArray['updated_at'] = date('Y-m-d H:i:s');
+            $this->Auth_model->create($formArray);
+
+            $this->session->set_flashdata('msg', 'Your account has been created successfully.');
+            redirect(base_url().'index.php/Auth/register');
+
+        }      
+        
+    }
+}
---
+++ application/views/register.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Register</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/style.css';?>">
+  </head>
+<body>
+
+<div class="container">
+  <?php
+      $msg = $this->session->flashdata('msg');
+      if($msg != "") {
+        echo "<div class='alert alert-success'>$msg</div>";
+      }  
+  ?>
+  <div class="col-md-6">
+    <div class="card mt-4">
+      <div class="card-header">
+         Register Here
+      </div>
+      <form action="<?php echo base_url().'index.php/Auth/register'?>" name="registerForm" id="registerForm" method="post">
+        <div class="card-body register">
+          <p class="card-text">Please fill with your details</p>
+          <div class="form-group">
+            <label for="name">First Name</label>
+            <input type="text" name="first_name" id="first_name" class="form-control <?php echo (form_error('first_name') != "") ? 'is-invalid' : '';?>" value="<?php echo set_value('first_name')?>" placeholder="First Name">
+            <p class="invalid-feedback"><?php echo strip_tags(form_error('first_name'));?></p>
+          </div>
+          <div class="form-group">
+            <label for="name">Last Name</label>
+            <input type="text" name="last_name" id="last_name" class="form-control <?php echo (form_error('last_name') != "") ? 'is-invalid' : '';?>" value="<?php echo set_value('last_name')?>" placeholder="Last Name">
+            <p class="invalid-feedback"><?php echo strip_tags(form_error('last_name'));?></p>
+          </div>
+          <div class="form-group">
+            <label for="name">Email</label>
+            <input type="text" name="email" id="email" class="form-control <?php echo (form_error('email') != "") ? 'is-invalid' : '';?>" value="<?php echo set_value('email')?>" placeholder="Email">
+            <p class="invalid-feedback"><?php echo strip_tags(form_error('email'));?></p>
+          </div>
+          <div class="form-group">
+            <label for="name">Phone</label>
+            <input type="text" name="phone" id="phone" class="form-control <?php echo (form_error('phone') != "") ? 'is-invalid' : '';?>" value="<?php echo set_value('phone')?>" placeholder="Phone">
+            <p class="invalid-feedback"><?php echo strip_tags(form_error('phone'));?></p>
+          </div>
+          <div class="form-group">
+            <label for="name">Password</label>
+            <input type="password" name="password" id="password" class="form-control <?php echo (form_error('password') != "") ? 'is-invalid' : '';?>" value="<?php echo set_value('password')?>" placeholder="Password">
+            <p class="invalid-feedback"><?php echo strip_tags(form_error('password'));?></p>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-block btn-primary">REGISTER NOW</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+  </div>
+</div>
+    
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>               
+</body>
+</html>
