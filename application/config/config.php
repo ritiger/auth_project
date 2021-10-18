31ec61fd3813d2dd1bb9572835e3b455837f4eb8
ritiger <jinzhexing26@gmail.com>
2021-10-18 16:10:41 +0800

    third commit;

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/auth_project/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
